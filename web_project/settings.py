9d5817da69086dfc3dd33bf26c055e319dbc8164
Sheridan <116517473+I-choose-charmander@users.noreply.github.com>
2024-03-11 20:00:22 -0700

    Update settings.py

--- web_project/settings.py
+++ web_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-jad!=i(y4%71u)_!^1742^iyhs#dxqyu$79)zz%i4($%gqpkht
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
