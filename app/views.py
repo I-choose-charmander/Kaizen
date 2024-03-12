92d5bfa34d5b16404728dd35ab9ce10812504619
I-choose-charmander <sheridantechrequest@gmail.com>
2024-03-12 12:38:44 -0700

    added functionality to Macro calculations to be able inform daily macro range with an updateable feature to remove daily intake from calculations

--- app/views.py
+++ app/views.py
@@ -25,43 +25,97 @@ def calories(request):
 
 def f_results(request):
     income= int(request.GET.get('inc'))
+    ef1=request.GET.get('extraField1')
+    ef2=request.GET.get('extraField2')
+    if [x for x in (ef1,ef2) if x == '']:
 
-    if request.GET.get('breakdown') == "":
+        if request.GET.get('breakdown') == "":
 
-        personal = income * .2
-        saving = income * .3
-        essentials = income * .5
+            personal = income * .2
+            saving = income * .3
+            essentials = income * .5
 
-        answer = {"Personal":personal,"Saving":saving, "Essential":essentials}
+            answer = {"Personal":personal,"Saving":saving, "Essential":essentials}
 
 
-    return render(request, 'f_results.html', {'ans': answer})
+            return render(request, 'f_results.html', {'ans': answer})
    
 def m_results(request):
     feet = (request.GET.get('feet'))
     inches = (request.GET.get('inches'))
     weight = (request.GET.get('weight'))
     age = (request.GET.get('age'))
+    protien = (request.GET.get('d_protien'))
+    carbs = (request.GET.get('d_carb'))
+    fats = (request.GET.get('d_fat'))
 
-    if request.GET.get('breakdown') == "":
-        if not [x for x in (feet,inches,weight,age) if x == '']:
+    if not [x for x in (feet,inches,weight,age) if x == '']:
             Feet = int(feet)
             Inches = int(inches)
             Weight = int(weight)
             Age = int(age)
+           
 
-            kg = Weight * 2.2
+            kg = Weight / 2.205
             cm = (((Feet * 12)+ Inches)*2.54)
 
-            if request.GET.get('checkbox') == "on":
-                ans = 66.47 + (13.75 * kg) + (5.003 * cm) - (6.755 * Age)
-                return render(request, 'm_results.html', {'ans': ans})
             
-            ans = 665.1 + (9.563 * kg) + (1.850 * cm) - (4.676 * Age)
-            return render(request, 'm_results.html', {'ans': ans})
+            protienl = round(Weight * 1.2)
+            protienh = round(Weight * 2)
+            carbl = round(Weight * 5)
+            carbh = round(Weight * 8)
+            fatl = round(Weight * .4)
+            fath = round(Weight * .5)
 
-        ans = 'Error Please try again'
-        return render(request, 'm_results.html', {'ans': ans})
+            macro_range = {'Protien Low': protienl,'Protien High': protienh,
+                              'Carbohydrate Low': carbl, 'Carbohydrate High': carbh, 
+                              'Fats Low':fatl, 'Fats High': fath }
+
+            if request.GET.get('checkbox') == "on":
+                ans = round(66.47 + (13.75 * kg) + (5.003 * cm) - (6.755 * Age))
+                if [x for x in (protien,carbs,fats) if x == '']:
+                    return render(request, 'm_results.html', {'ans': ans,'range':macro_range})
+                else:
+                    Protien = int(protien)
+                    Carb= int(carbs)
+                    Fat= int(fats)
+
+                    daily_protien_low = protienl - Protien
+                    daily_protien_high = protienh - Protien
+                    daily_carb_low = carbl - Carb
+                    daily_carb_high = carbh - Carb
+                    daily_fat_low = fatl - Fat
+                    daily_fat_high = fath - Fat
+                    
+                    macro_range = {'Protien Low': daily_protien_low,'Protien High': daily_protien_high,
+                              'Carbohydrate Low': daily_carb_low, 'Carbohydrate High': daily_carb_high, 
+                              'Fats Low':daily_fat_low, 'Fats High': daily_fat_high }
+                    return render(request, 'm_results.html', {'ans': ans,'range':macro_range})
+
+            
+            ans = round(665.1 + (9.563 * kg) + (1.850 + cm) - (4.676 * Age))
+            if [x for x in (protien,carbs,fats) if x == '']:
+                return render(request, 'm_results.html', {'ans': ans,'range':macro_range})
+            else:
+                Protien = int(protien)
+                Carb= int(carbs)
+                Fat= int(fats)
+
+                daily_protien_low = protienl - Protien
+                daily_protien_high = protienh - Protien
+                daily_carb_low = carbl - Carb
+                daily_carb_high = carbh - Carb
+                daily_fat_low = fatl - Fat
+                daily_fat_high = fath - Fat
+                macro_range = {'Protien Low': daily_protien_low,'Protien High': daily_protien_high,
+                              'Carbohydrate Low': daily_carb_low, 'Carbohydrate High': daily_carb_high, 
+                              'Fats Low':daily_fat_low, 'Fats High': daily_fat_high }
+                return render(request, 'm_results.html', {'ans': ans,'range':macro_range})
+
+    ans = 'Error Please try again'
+    macro_range= {'Na':0}
+    return render(request, 'm_results.html', {'ans': ans,'range':macro_range })
+         
 
 def c_results(request):
     cal = 0
